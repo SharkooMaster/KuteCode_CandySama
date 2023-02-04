a1dbd7151d5a1f2de569099a22a62cea95ac1eea
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2023-02-04 03:20:19 +0100

    Initial

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+import Home from './Screens/Home';
+
+const App = () =>
+{
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
