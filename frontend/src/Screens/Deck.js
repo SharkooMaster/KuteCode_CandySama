488a204c5d75c165457d19ea53e07bad9e6ee63b
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2023-02-07 00:14:20 +0100

    Cards page
    None of the current pages including this page are finished or polished. Neither are they responsive. they're placeholder until the design is tweaked

--- frontend/src/Screens/Deck.js
+++ frontend/src/Screens/Deck.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+
+// Animations
+import { StyleSheet, css } from 'aphrodite';
+import { fadeInUp } from "react-animations"
+
+// Images
+import TwoOfClubs from "../GFX/TwoOfClubs.png";
+import ThreeOfClubs from "../GFX/ThreeOfClubs.png";
+import FourOfClubs from "../GFX/FourOfClubs.png";
+import FiveOfClubs from "../GFX/FiveOfClubs.png";
+import SixOfClubs from "../GFX/SixOfClubs.png";
+
+import QueenOfClubs from "../GFX/QueenOfClubs.png";
+import JackOfClubs from "../GFX/JackOfClubs.png";
+
+// Components
+import Master from './Master';
 
 export default function Deck() {
+
+  const [_cards, SetCards] = useState([
+    TwoOfClubs,
+    ThreeOfClubs,
+    FourOfClubs,
+    FiveOfClubs,
+    SixOfClubs,
+    QueenOfClubs,
+    JackOfClubs
+  ]);
+
+  const [activeCard, setActiveCard] = useState(0);
+
+  const Card = () =>
+  {
+    const ret = _cards.map((i,k) =>
+      <img key={k} src={i} className="drop-shadow-md w-[150px] hover:drop-shadow-xl" onClick={()=>{setActiveCard(k)}} />
+    )
+    return(
+      ret
+    )
+  }
+
   return (
-	<div>Deck</div>
+    <Master light={true}>
+      <div className='flex w-full h-screen items-center justify-evenly pt-[70px] bg-gradient-to-t to-[#151425b4] from-[#09080F]'>
+        <div className='bg-white flex flex-col items-center shadow-md w-3/6 rounded-lg'>
+          <p className='text-2xl'>Suit</p>
+          <div className='flex flex-wrap w-full justify-evenly gap-2 py-8'>
+            {Card()}
+          </div>
+        </div>
+
+        <div className='bg-white flex flex-col items-center justify-center shadow-md w-2/6 py-8 rounded-lg'>
+          <img src={_cards[activeCard]} className="drop-shadow-md" />
+        </div>
+
+      </div>
+    </Master>
   )
 }
--- frontend/src/Screens/Master.js
+++ frontend/src/Screens/Master.js
@@ -1,17 +1,23 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
 import Logo from "../GFX/Logo.png";
+import LogoLight from "../GFX/LogoLight.png";
 import { Link } from 'react-router-dom';
 
 export default function Master(props) {
 	return (
 		<div>
 			<div className='absolute flex flex-row w-full h-[60px] items-center justify-between px-12'>
-				<img src={Logo} className="w-[35px] h-[40px] " />
+				{
+					(props.light) ?
+						<img src={LogoLight} className="w-[35px] h-[40px] " />
+					:
+						<img src={Logo} className="w-[35px] h-[40px] " />
+				}
 				<div className='flex flex-row h-[40] items-center gap-4'>
-					<Link to="/Deck"><p className='text-[18px]'>Deck</p></Link>
-					<Link to="/pre-order"><p className='text-[18px]'>Preorder</p></Link>
-					<Link to="/urmom"><p className='text-[18px]'>ur mom</p></Link>
+					<Link to="/Deck">		<p className={[(props.light) ? "text-white" : "" + ' text-[18px]']}>Deck</p> 	</Link>
+					<Link to="/pre-order">	<p className={[(props.light) ? "text-white" : "" + ' text-[18px]']}>Preorder</p>	</Link>
+					<Link to="/urmom">		<p className={[(props.light) ? "text-white" : "" + ' text-[18px]']}>ur mom</p>	</Link>
 				</div>
 			</div>
 
